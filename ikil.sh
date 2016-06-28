6b5f0e277041a41c229e67bef85e89287e6666b3
JvdS <jvds@jvds-mbp.local>
2016-06-28 15:26:12 +0200

    cosmetics

--- ikil.sh
+++ ikil.sh
@@ -1,14 +1,20 @@
 # simple bash script to kill a CF runtime instance of an app
 # usage : ikil.sh <appname> <instance, defaults to 0>
 
-instance=$2
-if [ "$instance" = "" ]; 
-then
-    instance="0"
-fi
-echo "Killing instance $instance of application $1"
 appName=$1
-appIDLine=`CF_TRACE=true cf app $appName | grep -E \"url\" | grep -E apps`
-tmp=${appIDLine#*/}
-appID=${tmp%\"*}
-cf curl /$appID/instances/$instance -X 'DELETE'
+instance=$2
+if [ "$appName" = "" ]; 
+then
+    echo "usage : ikil.sh application_name [instance]"
+else
+
+	if [ "$instance" = "" ]; 
+	then
+	    instance="0"
+	fi
+	echo "Killing instance $instance of application $appName"
+	appIDLine=`CF_TRACE=true cf app $appName | grep -E \"url\" | grep -E apps`
+	tmp=${appIDLine#*/}
+	appID=${tmp%\"*}
+	cf curl /$appID/instances/$instance -X 'DELETE'
+fi
